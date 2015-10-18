d6e34103b33598d4e3ffda909ec01cdd7acd131d
Luis Martinez <lwis851@gmail.com>
2015-10-18 16:19:55 -0600

    subiendo tarea 3

---
+++ tarea/tarea3.py
@@ -0,0 +1,17 @@
+# tarea 3 luis Martinez
+
+
+
+numero =(input("Ingrese numero:"))
+
+counter = 0
+
+
+while counter < int(numero):
+   counter += 1
+   r = (counter * (counter + 1)) / 2
+
+   print("%d - %d" % (counter, r))
+
+
+
