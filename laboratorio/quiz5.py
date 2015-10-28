2ba5bb8d6b47ffb5c9812b761c35386c41461cbd
Luis Martinez <lwis851@gmail.com>
2015-10-28 15:32:03 -0600

    subiendo quiz 5

---
+++ laboratorio/quiz5.py
@@ -0,0 +1,33 @@
+__author__ = 'luis M'
+# programa de super mercado
+print("   !**SUPER MERCADO**!")
+lista= []
+opcion=0;
+contador = -1
+
+while opcion != "4":
+ print("1.  Agregar producto")
+ print("2.  Eliminar producto")
+ print("3.  Productos en lista ")
+ print("4.  Salir")
+
+ opcion=input("   Elija una opcion: ")
+
+ if opcion == "1":
+  objeto=input("Que quieres agregar? ")
+  contador = contador + 1
+  objeto = (str(contador) + "." + str(objeto))
+  lista.append(objeto)
+
+ if opcion == "2":
+  print(lista)
+  eliminar = int(input("Que quieres eliminar de la lista? "))
+  print("Creo que ya se a eliminado : " + lista[eliminar])
+  del lista[eliminar]
+
+ if opcion == "3":
+  print(lista)
+
+ print("toca enter para continuar")
+ input()
+ os.system("cls")
